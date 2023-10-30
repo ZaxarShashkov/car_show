0b7f69b8872b41733f02e1863f2a0d2e31c0a5ec
ZaxarShashkov <zekharjah@gmail.com>
2023-10-30 13:27:17 +0300

    add car angle   && api

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { CarProps } from '@/interfaces/CarProps';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { CarDetails, CustomButton } from '.';
@@ -29,7 +29,13 @@ const CarCard = ({ car }: CarCardProps) => {
 				<span className='self-end text-[14px] font-medium'>/day</span>
 			</p>
 			<div className='relative w-full h-40 my-3 object-contain'>
-				<Image src='/hero.png' alt='car model' fill priority className='object-contain' />
+				<Image
+					src={generateCarImageUrl(car)}
+					alt='car model'
+					fill
+					priority
+					className='object-contain'
+				/>
 			</div>
 			<div className='relative flex w-full mt-2'>
 				<div className='flex group-hover:invisible w-full justify-between text-gray'>
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from '@/interfaces/CarProps';
+
 export async function fetchCars() {
 	const headers = {
 		'X-RapidAPI-Key': '0c81838904msh26bef6f1235c72fp1747e0jsnb0c30f119e93',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
 	};
 	const response = await fetch(
-		'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+		'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
 		{
 			headers,
 		}
@@ -12,6 +14,20 @@ export async function fetchCars() {
 	const result = await response.json();
 	return result;
 }
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+	const url = new URL('https://cdn.imagin.studio/getimage');
+
+	const { make, year, model } = car;
+	url.searchParams.append('customer', 'hrjavascript-mastery');
+	url.searchParams.append('make', make);
+	url.searchParams.append('modelFamily', model.split(' ')[0]);
+	url.searchParams.append('zoomType', 'fullscreen');
+	url.searchParams.append('modelYear', `${year}`);
+	url.searchParams.append('angle', `${angle}`);
+
+	return `${url}`;
+};
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
 	const basePricePerDay = 50;
